12378131f7d66a859ed84e27392ccb6e6c62a960
Shashwa7 <rextrip4@gmail.com>
2021-05-03 08:56:17 +0530

    Initial commit 3-5-21 Devops Lab Exam!

---
+++ Exp_2_lab_exam_3_5_21.java
@@ -0,0 +1,8 @@
+public class Exp_2_lab_exam_3_5_21
+{
+       public static void main (String[] args)
+       {
+             // Ausgabe Hello World!
+             System.out.println("This is the second lab experiment for Devops lab exam.");
+       }
+}
